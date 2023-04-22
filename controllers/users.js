30cf89ac0058613f1194d6208ab4e8ca38352099
sibbe23 <sibbe23@gmail.com>
2023-04-22 22:59:14 +0530

    Completed frontend design and some tweaks

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const User=require('../models/users');
 
 const bcrypt=require('bcrypt');
-
+const jwt=require('jsonwebtoken');
 
 function isStringInvalid(string){
     if(string==undefined ||string.length===0){
@@ -11,6 +11,39 @@ function isStringInvalid(string){
         return false;
     }
 }
+
+function generateAccessToken(id,name){
+
+    return jwt.sign({userId:id,name:name},process.env.TOKEN_SECRET);
+
+ }     
+
+ exports.login= async (req,res,next)=>{
+    try{
+        const{email,password}=req.body;
+        const user= await User.findAll({where:{email:email}});
+        //console.log(user);
+        if(user.length>0)
+        {
+        bcrypt.compare(password,user[0].password,(err,result)=>{
+            if(err){
+              throw new Error("Something Went Wrong");
+            }
+            if(result===true){
+                return res.status(200).json({success:true,message:"User Logged in Successfully",token:generateAccessToken(user[0].id,user[0].username)});
+            }
+                else{
+                return res.status(401).json({success:false,message:"Password is invalid"});
+             }
+        })   
+        }
+        else{
+            return res.status(404).json({success:false,message:"User does Not Exist"});
+        }
+    }catch(err){
+        return res.status(500).json({success:false,message:err});
+    }     
+    }
 
 exports.addUsers=async(req,res,next)=>{
     try{  
@@ -29,22 +62,16 @@ exports.addUsers=async(req,res,next)=>{
         res.status(201).json({message:"Successfully Created New User"});
         }
         catch(err){
-        if(err.name="SequelizeUniqueConstraintError"){
+        if(err){
            err="User Already Exists!  Please Login";
         } 
         else{
-        err="OOPS! Something Went wrong";
-        }
-            res.status(500).json({
-                message:err
-            });
+             err="OOPS! Something Went wrong"; }
+            res.status(500).json({ message:err});
         }  
     })
     }
     catch(err){
-        /* console.log(err); */
-        res.status(500).json({
-            message:err
-        });
+        res.status(500).json({ message:err });
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const userControllers=require('../controllers/users.js');
 const router=express.Router();
 
 router.post('/signup',userControllers.addUsers);
+router.post('/login',userControllers.login);
 
 module.exports=router;
